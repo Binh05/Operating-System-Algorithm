eb38e96831ab966cec0b4adec908caa9a7a2b084
Binh05 <binhdt0452@gmail.com>
2025-05-24 23:20:50 +0700

    update 2 PRIORITY and OPT

--- CK_HDH/CPU_Distribution.Designer.cs
+++ CK_HDH/CPU_Distribution.Designer.cs
@@ -29,36 +29,214 @@
         private void InitializeComponent()
         {
             this.Exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.cbThuatToan = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.dgvTienTrinh = new System.Windows.Forms.DataGridView();
+            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ArrivalTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BurstTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priority = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panelGanttChart = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTienTrinh)).BeginInit();
             this.SuspendLayout();
             // 
             // Exit
             // 
             this.Exit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Exit.Location = new System.Drawing.Point(610, 384);
+            this.Exit.Location = new System.Drawing.Point(610, 408);
             this.Exit.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(186, 63);
+            this.Exit.Size = new System.Drawing.Size(186, 67);
             this.Exit.TabIndex = 1;
             this.Exit.Text = "Thoát";
             this.Exit.UseVisualStyleBackColor = true;
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(260, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mô Phỏng Phân Phối Tiến Trình";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhập số tiến trình";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(372, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bảng Dữ Liệu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(471, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Chọn thuật toán ";
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(236, 90);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(77, 25);
+            this.txtP.TabIndex = 6;
+            // 
+            // cbThuatToan
+            // 
+            this.cbThuatToan.FormattingEnabled = true;
+            this.cbThuatToan.Location = new System.Drawing.Point(624, 90);
+            this.cbThuatToan.Name = "cbThuatToan";
+            this.cbThuatToan.Size = new System.Drawing.Size(211, 25);
+            this.cbThuatToan.TabIndex = 7;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(720, 158);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(94, 40);
+            this.btnLoad.TabIndex = 8;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(820, 158);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(94, 40);
+            this.btnRun.TabIndex = 9;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // dgvTienTrinh
+            // 
+            this.dgvTienTrinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTienTrinh.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PID,
+            this.ArrivalTime,
+            this.BurstTime,
+            this.Priority});
+            this.dgvTienTrinh.Location = new System.Drawing.Point(34, 205);
+            this.dgvTienTrinh.Name = "dgvTienTrinh";
+            this.dgvTienTrinh.Size = new System.Drawing.Size(880, 275);
+            this.dgvTienTrinh.TabIndex = 10;
+            // 
+            // PID
+            // 
+            this.PID.HeaderText = "PID";
+            this.PID.Name = "PID";
+            // 
+            // ArrivalTime
+            // 
+            this.ArrivalTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ArrivalTime.HeaderText = "ArrivalTime";
+            this.ArrivalTime.Name = "ArrivalTime";
+            // 
+            // BurstTime
+            // 
+            this.BurstTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.BurstTime.HeaderText = "BurstTime";
+            this.BurstTime.Name = "BurstTime";
+            // 
+            // Priority
+            // 
+            this.Priority.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Priority.HeaderText = "Priority";
+            this.Priority.Name = "Priority";
+            // 
+            // panelGanttChart
+            // 
+            this.panelGanttChart.Location = new System.Drawing.Point(34, 507);
+            this.panelGanttChart.Name = "panelGanttChart";
+            this.panelGanttChart.Size = new System.Drawing.Size(880, 82);
+            this.panelGanttChart.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(800, 597);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 43);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // CPU_DistributionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 534);
+            this.ClientSize = new System.Drawing.Size(949, 653);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panelGanttChart);
+            this.Controls.Add(this.dgvTienTrinh);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cbThuatToan);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Exit);
-            this.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "CPU_DistributionForm";
             this.Text = "CPU Distribution";
+            this.Load += new System.EventHandler(this.CPU_DistributionForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTienTrinh)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.ComboBox cbThuatToan;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.DataGridView dgvTienTrinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ArrivalTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BurstTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Priority;
+        private System.Windows.Forms.Panel panelGanttChart;
+        private System.Windows.Forms.Button button1;
     }
 }
--- CK_HDH/Page_Replacement.cs
+++ CK_HDH/Page_Replacement.cs
@@ -17,11 +17,137 @@ namespace CK_HDH
             InitializeComponent();
         }
 
+        List<int> referenceString = new List<int>();
+
         private void Exit_Click(object sender, EventArgs e)
         {
             var form1 = (Form1)Tag;
             form1.Show();
             Close();
         }
+
+        private void Page_ReplacementForm_Load(object sender, EventArgs e)
+        {
+            cbAlgorithm.Items.Add("OPT");
+            cbAlgorithm.Items.Add("LRU");
+            cbAlgorithm.Items.Add("FIFO");
+            cbAlgorithm.Items.Add("CLOCK");
+            cbAlgorithm.SelectedIndex = 0;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtFrame.Text, out int frames) || frames <= 0)
+            {
+                MessageBox.Show("Số frame không hợp lệ.");
+                return;
+            }
+
+            if (cbAlgorithm.SelectedItem?.ToString() == "OPT")
+            {
+                RunOptimal(frames, referenceString);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNum.Text, out int page))
+            {
+                referenceString.Add(page);
+                lblArray.Text = string.Join(" ", referenceString); // Ghi đè chuỗi
+                txtNum.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số hợp lệ.");
+            }
+        }
+
+        private void RunOptimal(int frameCount, List<int> pages)
+        {
+            dgvSimulation.Rows.Clear();
+            dgvSimulation.Columns.Clear();
+
+            // Tạo cột tiêu đề cho từng thời điểm
+            for (int i = 0; i < pages.Count; i++)
+            {
+                dgvSimulation.Columns.Add($"col{i}", pages[i].ToString());
+            }
+
+            List<int?> frames = new List<int?>(new int?[frameCount]);
+            List<List<int?>> history = new List<List<int?>>();
+
+            int pageFaults = 0;
+
+            for (int t = 0; t < pages.Count; t++)
+            {
+                int page = pages[t];
+
+                if (frames.Contains(page))
+                {
+                    // Hit
+                }
+                else
+                {
+                    pageFaults++;
+
+                    if (frames.Contains(null))
+                    {
+                        int emptyIndex = frames.IndexOf(null);
+                        frames[emptyIndex] = page;
+                    }
+                    else
+                    {
+                        // Tìm frame cần thay theo OPT
+                        int replaceIndex = 0;
+                        int farthest = -1;
+
+                        for (int i = 0; i < frames.Count; i++)
+                        {
+                            int? f = frames[i];
+                            int nextUse = pages.FindIndex(t + 1, x => x == f);
+
+                            if (nextUse == -1)
+                            {
+                                replaceIndex = i;
+                                break;
+                            }
+                            else if (nextUse > farthest)
+                            {
+                                farthest = nextUse;
+                                replaceIndex = i;
+                            }
+                        }
+
+                        frames[replaceIndex] = page;
+                    }
+                }
+
+                // Lưu trạng thái frame tại thời điểm t
+                history.Add(new List<int?>(frames));
+            }
+
+            // Vẽ lên dgv
+            for (int i = 0; i < frameCount; i++)
+            {
+                dgvSimulation.Rows.Add();
+                dgvSimulation.Rows[i].HeaderCell.Value = $"Frame {i + 1}";
+
+                for (int j = 0; j < history.Count; j++)
+                {
+                    dgvSimulation.Rows[i].Cells[j].Value = history[j][i]?.ToString() ?? "";
+                }
+            }
+
+            // Thêm dòng page faults
+            dgvSimulation.Rows.Add();
+            dgvSimulation.Rows[frameCount].HeaderCell.Value = "Page Fault";
+            for (int j = 0; j < pages.Count; j++)
+            {
+                bool isFault = (j == 0 || !history[j - 1].Contains(pages[j]));
+                dgvSimulation.Rows[frameCount].Cells[j].Value = isFault ? "F" : "";
+            }
+        }
+
     }
 }
